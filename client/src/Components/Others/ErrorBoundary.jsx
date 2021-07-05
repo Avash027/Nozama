5259bb5c4a71a2dff8973550d6568ea2e9580fe2
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2021-07-05 19:34:41 +0530

    Few changes before deploying

--- client/src/Components/Others/ErrorBoundary.jsx
+++ client/src/Components/Others/ErrorBoundary.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React from "react";
 
 export default class ErrorBoundary extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { hasError: false };
-    }
-  
-    componentDidCatch(error) {
-      this.setState({ hasError: true });
-      console.log(error);
-    }
-  
-    render() {
-      if (this.state.hasError) {
-        return (
-        <div className="error-body">
-        <p className="error-info">&gt;Oh No! Nozama is sad! Internally sad!</p>
-        <h1 className="internal"><span className="five">5</span><span className="zero">0</span><span className="zero">0</span></h1>
-        <footer className="error-link"><a href="https://www.altreiter.at">altreiter.at</a></footer>
-        </div>
-
-        )
-        ;
-      }
-      return this.props.children;
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
   }
+
+  componentDidCatch(error) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="error-body">
+          <p className="error-info">
+            &gt;Oh No! Nozama is sad! Internally sad!
+          </p>
+          <h1 className="internal">
+            <span className="five">5</span>
+            <span className="zero">0</span>
+            <span className="zero">0</span>
+          </h1>
+          <footer className="error-link">
+            <a href="/">Got to main page</a>
+          </footer>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
